6c99b22ef75eb88755e9e92fe11963a55e11040b
Rodrigues-PauloRicardo <rodrigues.pauloricardo@gmail.com>
2023-02-06 18:39:58 -0300

    at

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ app.listen(PORT, () => {           // escutar eventos para porta
     console.log(`Servidor rodando no http://localhost:${PORT}`)
 })
 
+const Sequelize = require('sequelize');
+
+//CONEXÃO COM O DB
+const connection = new Sequelize('guiaperguntas','root','123456',{
+    host: 'localhost', // onde se encontra o DB
+    dialect: 'mysql' // qual o tipo de banco de dados
+   
+});// Nome do Banco de Dados - root q é padrao - senha
+
+
+
+module.exports = connection;
